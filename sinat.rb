8e53d26d11ed6f1b384d5014b6f5ae735441bbd2
Will Strinz <wstrinz@gmail.com>
2013-03-02 16:57:29 -0600

    prevent model from running more than once

--- sinat.rb
+++ sinat.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+configure do
+  @@started_s = false
+end
+
 get '/' do
   "Biofuels Game Model \n If you just woke up the server, try '/start' to get the model running again"
 end
@@ -9,9 +13,14 @@ get '/hi' do
 end
 
 get '/start' do
-  Thread.new do
-    puts load('javaGame/server_runner.rb')
+  unless @@started_s
+    Thread.new do
+      puts load('javaGame/server_runner.rb')
+    end
+    @@started_s = true
+    "Started server. Don't go running this twice now..."
+  else
+    "Server already started!"
   end
-  "Started server. Don't go running this twice now..."
 end
 
